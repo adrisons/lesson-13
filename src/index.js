5def1c997b06da2c7c68f33f0130feb3fa492484
Adrián Pereira Guerra <adries.91@gmail.com>
2021-07-30 11:02:15 +0200

    fix: store export persistor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-
-import { store, persistor } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
-import "./index.css";
 import App from "./App";
+import "./index.css";
+import { persistor, store } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
